6274714d329ee1731f697841cc3061c1e3d06250
hongfei <hongfei.tian@zhangmen.com>
2019-05-02 13:27:59 +0800

    quanxian

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,40 @@
 <?php
 namespace app\index\controller;
 
-class Index
+class Index extends \think\Controller
 {
     public function index()
     {
         return 'nima';
+    }
+    public function home()
+    {
+        return 'home';
+    }
+    public function upload()
+    {
+        return $this->fetch('upload/index');
+    }
+    public function uploads(){
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('image');
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+            if($info){
+                // 成功上传后 获取上传信息
+                // 输出 jpg
+                echo $info->getExtension();
+                // echo '<br>';
+                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getSaveName();
+                // echo '<br>';
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getFilename(); 
+            }else{
+                // 上传失败获取错误信息
+                echo $file->getError();
+            }
+        }
     }
 }
--- application/route.php
+++ application/route.php
@@ -1,21 +1,13 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
-
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
-
-];
+/**
+ * Are you ok?
+ */
+use think\Route;
+Route::get([
+    '/' =>  'Index/home',
+    'new/:id'  =>  'News/read',
+    '/upload' =>  'Index/upload'
+]);
+Route::post([
+    '/uploads' =>  'Index/uploads'
+]);

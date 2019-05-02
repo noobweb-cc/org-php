26580a48849001da8ad1e247731dca7b9b96481a
hongfei <hongfei.tian@zhangmen.com>
2019-05-02 14:04:47 +0800

    返回上传的路径

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,19 +18,18 @@ class Index extends \think\Controller
     public function uploads(){
         // 获取表单上传文件 例如上传了001.jpg
         $file = request()->file('image');
-        // 移动到框架应用根目录/public/uploads/ 目录下
         if($file){
-            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+            // 移动到框架应用根目录/public/uploads/ 目录下
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'upfiles');
             if($info){
                 // 成功上传后 获取上传信息
-                // 输出 jpg
-                echo $info->getExtension();
-                // echo '<br>';
-                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
-                echo $info->getSaveName();
-                // echo '<br>';
-                // 输出 42a79759f284b767dfcb2a0197904287.jpg
-                echo $info->getFilename(); 
+                echo $info->getExtension(); // 文件后缀
+                echo $info->getSaveName(); // 文件路径
+                echo $info->getFilename(); // 文件名称
+                echo '<br>';
+                echo ROOT_PATH . 'public' . DS . 'upfiles' . $info->getSaveName();
+                echo '<br>';
+                echo 'public' . DS . 'upfiles'. DS . $info->getSaveName();
             }else{
                 // 上传失败获取错误信息
                 echo $file->getError();
